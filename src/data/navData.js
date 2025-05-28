8688b1f80c8805ccc982fb8dbfb989bac5551101
Florian Bertholin <arkan@users.noreply.github.com>
2025-05-28 11:51:09 +0200

    Initial commit

---
+++ src/data/navData.js
@@ -0,0 +1,29 @@
+const navData = [
+    {
+        name: "HOME",
+        path: "/#home",
+    },
+    {
+        name: "ABOUT",
+        path: "/#about",
+    },
+    {
+        name: "PUBLICATIONS",
+        path: "/#publications",
+    },
+    {
+        name: "WORK IN PROGRESS",
+        path: "/#work-in-progress",
+    },
+    {
+        name: "RESEARCH INTERESTS",
+        path: "/#research-interests",
+    },
+    {
+        name: "TEACHING",
+        path: "/#teaching",
+    },
+]
+
+export default navData;
+
