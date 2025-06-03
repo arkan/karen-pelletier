74391b562d4f985f3591a2c3101d6fb3ed1aa9dd
Florian Bertholin <arkan@users.noreply.github.com>
2025-06-03 23:26:46 +0200

    polished the design

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,22 +1,9 @@
 import rss from '@astrojs/rss';
 import siteData from "../data/siteData.json";
 
-import { formatBlogPosts } from "../js/utils"
-
-const postImportResult = import.meta.glob('./posts/**/*.md', { eager: true });
-const posts = formatBlogPosts(Object.values(postImportResult));
-
 export const GET = () => rss({
   title: siteData.title,
   description: siteData.description,
   site: import.meta.env.SITE,
-  items: posts.map((post) => ({
-    link: post.url,
-    title: post.frontmatter.title,
-    pubDate: post.frontmatter.date,
-    description: post.frontmatter.description,
-    customData: `
-      <author>${siteData.author}</author>
-    `
-  }))
+  items: []
 });
